c484f2821c605ff9a8a89847d46b16a83907694c
Hector Shin Emura <hector.shin@gmail.com>
2022-12-13 19:33:06 -0300

    Navegação da tela de registro para conta

--- RegisterWindow.xaml.cs
+++ RegisterWindow.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Data.SqlClient;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -13,21 +13,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Cashin.ViewModels;
 
 namespace Cashin
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RegisterWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegisterWindow : Page
     {
-        public MainWindow()
+        public RegisterWindow()
         {
             InitializeComponent();
         }
-
-        private void Login_Click(object sender, RoutedEventArgs e)
+        private void RegisterDB_Click(object sender, RoutedEventArgs e)
         {
             string stringConexao = "Server=labsoft.pcs.usp.br; Initial Catalog=db_12; User Id=usuario_12; Password=9077879676;";
             SqlConnection con = new SqlConnection(stringConexao);
@@ -35,22 +33,28 @@ namespace Cashin
             {
                 if (con.State == System.Data.ConnectionState.Closed)
                     con.Open();
-                String query = "SELECT COUNT(*) FROM [db_12].[dbo].[User] WHERE ((email=@Username) AND (senha=@Password))";
+                String query = "SELECT COUNT(*) FROM [db_12].[dbo].[User] WHERE email=@Username";
                 SqlCommand sqlCmd = new SqlCommand(query, con);
                 sqlCmd.CommandType = System.Data.CommandType.Text;
-                sqlCmd.Parameters.AddWithValue("@Username", UsernameForm.Text);
-                sqlCmd.Parameters.AddWithValue("@Password", PasswordForm.Password);
+                sqlCmd.Parameters.AddWithValue("@Username", EmailForm.Text); //Username passado no WPF
                 int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
                 if (count == 1)
                 {
                     this.Opacity = 0.5;
-                    Sucess.IsOpen = true;
-                    
+                    AlreadyRegistered.IsOpen = true; //Popup
                 }
                 else
                 {
                     this.Opacity = 0.5;
-                    Fail.IsOpen = true;
+                    SucessRegister.IsOpen = true; // Popup
+                    String queryCreateUser = "INSERT INTO [db_12].[dbo].[User] VALUES (@Email, @Username, @Password)";
+                    SqlCommand sqlCmdRegisterUser = new SqlCommand(queryCreateUser, con);
+                    sqlCmdRegisterUser.CommandType = System.Data.CommandType.Text;
+                    sqlCmdRegisterUser.Parameters.AddWithValue("@Username", UsernameForm.Text); //Username passado no WPF
+                    sqlCmdRegisterUser.Parameters.AddWithValue("@Email", EmailForm.Text); //Username passado no WPF
+                    sqlCmdRegisterUser.Parameters.AddWithValue("@Password", PasswordForm.Password); //Senha passada no WPF
+                    sqlCmdRegisterUser.ExecuteScalar();
+
                 }
             }
             catch (Exception ex)
@@ -62,24 +66,15 @@ namespace Cashin
                 con.Close();
             }
         }
-
         private void Close_Click(object sender, RoutedEventArgs e)
         {
-            AccountWindow page = new AccountWindow();
-            this.Content = page;
-            Sucess.IsOpen = false;
-            Fail.IsOpen = false;
+            if (SucessRegister.IsOpen == true) {
+                AccountWindow page = new AccountWindow();
+                this.Content = page;
+            };
+            SucessRegister.IsOpen = false;
+            AlreadyRegistered.IsOpen = false;
             this.Opacity = 1;
-        }
-        private void Register_Click(object sender, RoutedEventArgs e)
-        {
-            RegisterWindow page = new RegisterWindow();
-            this.Content = page;
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
