b9e6cd82fa60233aa18f89d84757ae3b0156e6f4
Hector Shin Emura <hector.shin@gmail.com>
2022-12-14 09:07:55 -0300

    Finalizando AddItem

---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashin.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public float Valor { get; set; } = 0;
+        public string Descricao { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;
+        public string Categoria { get; set; } = string.Empty;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashin.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty ;
+        public string Email { get; set; } = string.Empty ;
+        public float Saldo { get; set; } = 0;
+        public float Limite { get; set; } = 0;
+    }
+}
